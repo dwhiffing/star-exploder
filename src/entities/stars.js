72ef47d3995ee289631a47015e2d008a08bfe682
Daniel Whiffing <danielwhiffing@gmail.com>
2021-08-22 12:47:49 -0400

    Functional starfield

--- src/entities/stars.js
+++ src/entities/stars.js
@@ -1,23 +1,44 @@
 import { Sprite, Pool } from 'kontra'
 
 export const Stars = () => {
-  let pool = Pool({ create: Sprite, maxSize: 1000 })
+  let pool = Pool({ create: Sprite, maxSize: STAR_COUNT })
+  let lastCoords = {}
+  for (let i = 0; i < STAR_COUNT; i += 1) {
+    pool.get({ x: 0, y: 0, anchor: { x: 0.5, y: 0.5 } })
+  }
+
   return {
     pool,
-    add: (x, y, chunkIndex) =>
-      pool.get({
-        x,
-        y,
-        anchor: { x: 0.5, y: 0.5 },
-        color: COLORS[chunkIndex],
-        width: 20,
-        height: 20,
-      }),
     destroy() {
       pool.clear()
     },
-    update() {
+    update(x, y, chunkSize) {
       pool.update()
+      const chunkX = Math.floor(x / chunkSize)
+      const chunkY = Math.floor(y / chunkSize)
+
+      if (lastCoords.x === chunkX && lastCoords.y === chunkY) return
+      lastCoords = { x: chunkX, y: chunkY }
+
+      pool.objects.forEach((star, index) => {
+        let chunkIndex = Math.floor(index / (STAR_COUNT / 9))
+        const _x = COORDS[chunkIndex][0] + chunkX
+        const _y = COORDS[chunkIndex][1] + chunkY
+
+        const seedBase = `seed-${_x},${_y},${index % (STAR_COUNT / 9)}`
+        const x = (hashCode(`${seedBase}x`) % 100) / 100
+        const y = (hashCode(`${seedBase}thatsthey`) % 100) / 100
+        const alpha = (hashCode(`${seedBase}alpha`) % 100) / 100
+        const color = hashCode(`${seedBase}color`) % 10
+        const size = hashCode(`${seedBase}size`) % 5
+
+        star.x = _x * chunkSize + (x * chunkSize - chunkSize / 2)
+        star.y = _y * chunkSize + (y * chunkSize - chunkSize / 2)
+        star.opacity = alpha
+        star.color = COLORS[color]
+        star.width = size
+        star.height = size
+      })
     },
     render() {
       pool.render()
@@ -25,14 +46,40 @@ export const Stars = () => {
   }
 }
 
+const STAR_COUNT = 9 * 200
 const COLORS = [
   'white',
-  'red',
-  'blue',
-  'green',
+  'white',
+  'white',
+  'white',
+  'white',
+  'white',
+  'white',
   'yellow',
-  'purple',
-  'cyan',
-  'brown',
-  'gray',
+  'blue',
+  'red',
 ]
+const COORDS = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [0, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+]
+
+const hashCode = function (str) {
+  var hash = 0
+  if (str.length === 0) {
+    return hash
+  }
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charCodeAt(i)
+    hash = (hash << 5) - hash + char
+    hash = hash & hash
+  }
+  return Math.abs(hash)
+}
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -9,94 +9,21 @@ export const GameScene = ({ canvas }) => {
     id: 'game',
     children: [],
   })
-  for (let i = 0; i < 90; i += 1) {
-    let chunkIndex = Math.floor(i / (90 / 9))
-    scene.addChild(stars.add(0, 0, chunkIndex))
-  }
+
+  stars.pool.objects.forEach((o) => scene.addChild(o))
+
   scene.addChild(player.sprite)
-  const chunkSize = canvas.width / 3
-  let currentChunkX
-  let currentChunkY
 
   return {
     shutdown() {},
     update(dt) {
-      const chunkY = Math.floor(scene.camera.y / chunkSize)
-      const chunkX = Math.floor(scene.camera.x / chunkSize)
-      if (currentChunkX !== chunkX || currentChunkY !== chunkY) {
-        let objects = stars.pool.objects
-        let xdir =
-          chunkX === currentChunkX || !currentChunkX
-            ? null
-            : chunkX < currentChunkX
-            ? 'left'
-            : 'right'
-        let ydir =
-          chunkY === currentChunkY || !currentChunkY
-            ? null
-            : chunkY < currentChunkY
-            ? 'up'
-            : 'down'
-        objects.forEach((i, index) => {
-          let regenerate = false
-
-          let chunkIndex = Math.floor(index / (90 / 9))
-          let xOffset2 = 0
-          let yOffset2 = 0
-
-          if (xdir === 'left') {
-            regenerate = chunkIndex % 3 === 0
-            regenerate = chunkIndex === 5
-            xOffset2 = -1 * chunkSize
-            yOffset2 = 0
-          }
-
-          if (xdir === 'right') {
-            regenerate = chunkIndex % 3 === 2
-            xOffset2 = 1 * chunkSize
-            yOffset2 = 0
-          }
-
-          if (ydir === 'up') {
-            regenerate = chunkIndex === 7
-          }
-
-          if (ydir === 'down') {
-            regenerate = chunkIndex === 1
-          }
-
-          if (
-            typeof currentChunkX !== 'number' ||
-            typeof currentChunkY !== 'number'
-          ) {
-            regenerate = true
-          }
-
-          if (regenerate) {
-            const xOffset = (chunkIndex % 3) - 1
-            const yOffset = Math.floor(chunkIndex / 3) - 1
-            const _x = chunkSize
-            const _y = chunkSize
-            const X = 0.5 //Math.random()
-            const Y = 0.5 //Math.random()
-            i.x = _x * X + _x * xOffset + chunkX * chunkSize + xOffset2
-            i.y = _y * Y + _y * yOffset + chunkY * chunkSize + yOffset2
-            console.log(chunkIndex, xOffset, yOffset)
-          }
-        })
-
-        currentChunkX = chunkX
-        currentChunkY = chunkY
-        console.log('new chunk', chunkX, chunkY)
-      }
       player.update()
       scene.update()
       scene.lookAt(player.sprite)
-      stars.update()
+      stars.update(scene.camera.x, scene.camera.y, canvas.width)
     },
     render() {
       scene.render()
-      // stars.render()
     },
   }
 }
